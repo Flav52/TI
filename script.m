2cdc2efffb7e59f4df9bee0e9b65035187efe1f1
LeLunaa <38189825+LeLunaa@users.noreply.github.com>
2020-05-24 19:44:16 +0200

    Add files via upload

--- script.m
+++ script.m
@@ -7,16 +7,17 @@ tailleMarque = size(mess);
 itatouadapt = uint8(tatouageJstegAdaptatif(mess,I,fq));
 itatounrml = uint8(tatouageJsteg(mess,I,fq));
 
-
-
-PSNRnmrl = PSNR(I,itatounrml);
+PSNRnrml = PSNR(I,itatounrml);
 PSNRadap = PSNR(I,itatouadapt);
+Gain=((PSNRadap/PSNRnrml)-1)*100;
+disp('PSNR Jpeg/Jsteg classique'); disp(PSNRnrml);
+disp('PSNR Jpeg/Jsteg adaptatif'); disp(PSNRadap);
+disp('Gain'); disp(Gain);
 
-imshow(I);
-figure;
-imshow(itatounrml);
-figure;
-imshow(itatouadapt);
+subplot(1,3,1); imshow(I); title('Image de base');
+subplot(1,3,2); imshow(itatounrml); title('Image Jpeg/Jsteg classique');
+subplot(1,3,3); imshow(itatouadapt); title('Image Jpeg/Jsteg adaptatif');
+
 
 marquenmrl = getMarque(itatounrml,tailleMarque,fq);
 marqueadapt = getMarque(itatouadapt,tailleMarque,fq);
