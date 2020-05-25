ccd2f33af5f4746f037eef1c5a41f9e7b2e154b8
beltramem <beltramo.emeric@gmail.com>
2020-05-25 19:15:58 +0200

    lol on vire es trucs osef

--- test.m
+++
@@ -1,7 +0,0 @@
-i= [125 1 0 0 0 1 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0];
-zig = zigzag(i);
-rl = rle(zig);
-rl2 = rle(rl);
-
-symbole = unique(i);
-dd = compute_hufftree(zig);
